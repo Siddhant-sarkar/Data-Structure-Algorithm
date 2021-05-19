cc3204ca86094c658e2d1cba883c7e6f0e4496a3
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-05-19 21:21:06 +0530

    some more things are done!!!!

--- Algorithms/NumberTheory/gcdEulids.cpp
+++ Algorithms/NumberTheory/gcdEulids.cpp
@@ -1,12 +1,15 @@
 #include"bits/stdc++.h"
 using namespace std;
 
+//This is the non-recursive implementation of the 
+// euclids gcd algorithm
 int findGcd(int a,int b){
 	if(!b)
 		return a;
 	return findGcd(b,a%b);
 }
-
+//This is the recursive implementation of the 
+// euclids gcd algorithm
 int findGcdRec(int a,int b){
 	while(b){
 		a%=b;
@@ -14,6 +17,13 @@ int findGcdRec(int a,int b){
 	}
 	return a;
 }
+
+//If both numbers are even, then we can factor out a two of both and compute the GCD of the remaining numbers: gcd(2a,2b)=2gcd(a,b).
+
+//If one of the numbers is even and the other one is odd, then we can remove the factor 2 from the even one: gcd(2a,b)=gcd(a,b) if b is odd.
+
+//If both numbers are odd, then subtracting one number of the other one will not change the GCD: gcd(a,b)=gcd(b,a−b)
 int main(){
-	cout<<findGcdRec(4,10)<<endl;
+	cout<<gcd(4,10);
+	// cout<<findGcdRec(4,10)<<endl;
 }
--- DataStructures/2-DArray/search.cpp
+++ DataStructures/2-DArray/search.cpp
@@ -11,20 +11,18 @@ bool isThere(vector<vector<int>>ar,int s){
 	return false;
 }
 bool isThereNew(vector<vector<int>>ar,int s){
-	int hr=(ar[0].size())-1,vr=0;
-	while(hr>0;vr<ar.size()){
-		if(ar[vr][hr]==s)
-			return true;
-		else if(ar[vr][hr]>s)
-			vr++;
-		else 
-			hr--;
+	int n=ar[0].size()-1;
+	int c=ar[0].size()-1,r=0;
+	bool f=false;
+	while(r<n && c>=0){
+		if(s==ar[r][c]) f=true;
+		else if(s<ar[r][c]) c--;
+		else  r++;
 	}
-	return false;
-
+	return f;
 }
 int main(){
 	vector<vector<int>> ar={{1,2,3},{4,5,6},{7,8,9}};
-	isThereNew(ar,90)?cout<<"Yes\n":cout<<"No\n";
+	cout<<isThereNew(ar,8);
 	return 0;
 }
