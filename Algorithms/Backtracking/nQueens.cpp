cc3f5879f01c58621aa6f05b403c54bbefec4be7
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-05-24 16:06:07 +0530

    nQueens problem is easily done in I just don't know what the fuck i was doing

--- Algorithms/Backtracking/nQueens.cpp
+++ Algorithms/Backtracking/nQueens.cpp
@@ -3,17 +3,26 @@ using namespace std;
 vector<int>col(100,0);
 vector<int>dia1(100,0);
 vector<int>dia2(100,0);
-int count=0;
-void nqueen(vector<vector<int>>ans,int y,int n){
+void print(vector<vector<int>>ans){
+	cout<<"********\n";
+	for(auto it:ans){
+		for(auto iit :it){
+			cout<<iit<<"	";
+		}
+		cout<<endl;
+	}
+	cout<<"********\n";
+}
+void nqueen(vector<vector<int>>&ans,int y,int n){
 	if(y==n){
-		count++;
-		return;
+		print(ans);
+		return;	
 	}
 	for(int x=0;x<n;x++){
-		if(col[x] || dia1[x+y]|| dia2[x-y+n-1]) return;
+		if(col[x] || dia1[x+y]|| dia2[x-y+n-1]) continue;
 		col[x]=dia1[x+y]=dia2[x-y+n-1]=1;
 		ans[x][y]=1;
-		nqueen(y+1,n);
+		nqueen(ans,y+1,n);
 		col[x]=dia1[x+y]=dia2[x-y+n-1]=0;
 		ans[x][y]=0;
 	}
@@ -21,14 +30,6 @@ void nqueen(vector<vector<int>>ans,int y,int n){
 int main(){
 	int n;
 	cin>>n;
-	vector<vector<int>>ans(4,vector<int>(4,5));
-	for(auto it:ans){
-		for(auto iit: it){
-			cout<<iit<<"	";
-		}
-		cout<<endl;
-	}
-	// nqueen(ans,0,n);
-	cout<<count<<endl;
-	
+	vector<vector<int>>ans(n,vector<int>(n,0));
+	nqueen(ans,0,n);
 }
