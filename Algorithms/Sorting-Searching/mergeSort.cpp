bc46fdadd9fdb12cbee0afdbfa1e2c284df17f41
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-05-31 22:21:11 +0530

    more things on the way

--- Algorithms/Sorting-Searching/mergeSort.cpp
+++ Algorithms/Sorting-Searching/mergeSort.cpp
@@ -1,111 +1,54 @@
 #include"bits/stdc++.h"
 using namespace std;
-#define gc getchar_unlocked
-#define fo(i,n) for(i=0;i<n;i++)
-#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define ll long long
-#define si(x) scanf("%d",&x)
-#define sl(x) scanf("%lld",&x)
-#define ss(s) scanf("%s",s)
-#define pi(x) printf("%d\n",x)
-#define pl(x) printf("%lld\n",x)
-#define ps(s) printf("%s\n",s)
-#define ci(x) cin>>x
-#define ci2(x,y) cin>>x>>y
-#define ci3(x,y,z) cin>>x>>y>>z
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define sortall(x) sort(all(x))
-#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
 
-typedef pair<int, int>  pii;
-typedef pair<ll, ll>  pl;
-typedef vector<int>   vi;
-typedef vector<ll>    vl;
-typedef vector<pii>   vpii;
-typedef vector<pl>    vpl;
-typedef vector<vi>    vvi;
-typedef vector<vl>    vvl;
-
-
-
-const int mod = 1'000'000'007;
-const int N = 3e5, M = N;
-//=======================
-
-vi g[N];
-int a[N];
+void print(int ar[],int n){
+	for(int i=0;i<n;i++)
+		cout<<ar[i]<<"	";
+	cout<<endl;
+}
 void merge(int ar[],int l,int m,int r){
-	int n1=m-l+1,n2=r-m;
-
-	int a[n1],b[n2];
-	for(int i=0;i<n1;i++){
-		a[i]=ar[i+l];
+	int x=0,y=0,k=0;
+	int t1[m-l+1];
+	int t2[r-m];
+	// two half of the array have been copied
+	for(int i=0;i<m-l+1;i++){
+		t1[i]=ar[i];
 	}
-	for(int j=0;j<n2;j++){
-		b[j]=ar[j+1+m];
+	for(int i=0;i<r-m;i++){
+		t2[i]=ar[m+1+i];
 	}
-	int i=0,j=0,k=l;
-
-	while(i<n1&&j<n2){
-		if(a[i]<=b[j]){
-			ar[k]==a[i];
-			i++;
+	while(x<m-l+1 && y<r-m){
+		if(t1[x]<=t2[y]){
+			ar[k]=t1[x];
+			x++;
 		}else{
-			ar[k]=b[j];
-			j++;
+			ar[k]=t2[y];
+			y++;
 		}
 		k++;
 	}
+	while(x<m-l+1){
+		ar[k]=t1[x];
+		x++;
+		k++;
+	}
+	while(y<m-r){
+		ar[k]=t2[y];
+		y++;
+		k++;
+	}
 
-	while(i<n1){
-		ar[k]=a[i];
-		i++;
-		k++;
-	}
-	while(j<n2){
-		ar[k]=b[j];
-		j++;
-		k++;
-	}
 }
 void mergeSort(int ar[],int l,int r){
-	if(l<r){
 	int m=(l+r)/2;
-	mergeSort(ar,l,m);
-	mergeSort(ar,m+1,r);
-	merge(ar,l,m,r);
+	if(l<r){
+		mergeSort(ar,l,m);
+		mergeSort(ar,m+1,r);
+		merge(ar,l,m,r);
+	}
 }
-}
-void solve() {
-  int i, j, k;
-  int a ,b ,c ,d ,m ,n;
-  int ar[5]={5,4,3,2,1};
-
-  mergeSort(ar,0,4);
-  fo(i,5) cout<<ar[i]<<"   ";
-}
-
-int main() {
-
- #ifndef ONLINE_JUDGE
-    freopen("inp.txt","r",stdin);
-    freopen("out.txt","w",stdout);
-    freopen("err.txt","w",stderr);
-  #endif
-
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int t = 1;
-    // cin >> t;
-    while(t--) {
-      solve();
-    }
-    cerr<<"time taken : "<<((float)clock()/CLOCKS_PER_SEC)*1000<<" msecs"<<endl;
-    return 0;
+int main(){
+	int ar[6]={4,3,2,5,2,4};
+	mergeSort(ar,0,6);
+	print(ar,6);
 }
--- Algorithms/Sorting-Searching/quickSort.cpp
+++ Algorithms/Sorting-Searching/quickSort.cpp
@@ -1,109 +1,34 @@
 #include"bits/stdc++.h"
-#include <cstdlib>
 using namespace std;
-#define gc getchar_unlocked
-#define fo(i,n) for(i=0;i<n;i++)
-#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define ll long long
-#define si(x) scanf("%d",&x)
-#define sl(x) scanf("%lld",&x)
-#define ss(s) scanf("%s",s)
-#define pi(x) printf("%d\n",x)
-#define pl(x) printf("%lld\n",x)
-#define ps(s) printf("%s\n",s)
-#define ci(x) cin>>x
-#define ci2(x,y) cin>>x>>y
-#define ci3(x,y,z) cin>>x>>y>>z
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define sortall(x) sort(all(x))
-#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
 
-typedef pair<int, int>  pii;
-typedef pair<ll, ll>  pl;
-typedef vector<int>   vi;
-typedef vector<ll>    vl;
-typedef vector<pii>   vpii;
-typedef vector<pl>    vpl;
-typedef vector<vi>    vvi;
-typedef vector<vl>    vvl;
-
-
-
-const int mod = 1'000'000'007;
-const int N = 3e5, M = N;
-//=======================
-
-vi g[N];
-int a[N];
-///=======================///
-int partitionRandom(int ar[],int l,int r);
-int partition(int ar[],int l,int r);
-void quicksort(int ar[],int l,int r);
-int partitionGreator(int ar[],int l,int r);
-///=======================///
-
-
-
-
-int partitionRandom(int ar[],int l,int r){
-	srand(time(NULL));
-    int ra=l+rand() % (r-l);
-	swap(ar[ra],ar[r]);
-	return partitionGreator(ar,l,r);
+void print(int ar[],int n){
+	for(int i=0;i<n;i++)
+		cout<<ar[i]<<"	";
+	cout<<endl;
 }
 
-int partitionGreator(int ar[],int l,int r){
-	int p=ar[r],a ,i=l;
-	Fo(a,l,r){
-		if(ar[a]<=p){
-			swap(ar[a],ar[i]);
+int partition(int ar[],int l,int r){
+	int i=l-1;
+	int p=ar[r];
+	for(int j=l;j<r;j++)	{
+		if(ar[j]<p){
 			i++;
+			swap(ar[i],ar[j]);
 		}
 	}
-	swap(ar[i],ar[r]);
-	return i;
-
-
+	swap(ar[i+1],ar[r]);
+	return i+1;
 }
+
 void quicksort(int ar[],int l,int r){
 	if(l<r){
-		int p=partitionRandom(ar,l,r);
+		int p=partition(ar,l,r);
 		quicksort(ar,l,p-1);
 		quicksort(ar,p+1,r);
 	}
 }
-void solve() {
-  int i, j, k;
-  int a ,b ,c ,d ,m ,n;
-  int ar[5]={2,3,4,1,5};
-  quicksort(ar,0,4);
-  fo(i,5) pi(ar[i]);
-  // fo(i,100) cout<<rand()%10<<endl;
-
-
-}
-
-int main() {
-
- #ifndef ONLINE_JUDGE
-    freopen("inp.txt","r",stdin);
-    freopen("out.txt","w",stdout);
-    freopen("err.txt","w",stderr);
-  #endif
-
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int t = 1;
-    cin >> t;
-    while(t--) {
-      solve();
-    }
-    cerr<<"time taken : "<<((float)clock()/CLOCKS_PER_SEC)*1000<<" msecs"<<endl;
-    return 0;
+int main(){
+	int ar[6]={1,4,5,2,3,6};
+	quicksort(ar,0,4);
+	print(ar,6);
 }
