83362c9506209314bd2187780d778b9464613bf7
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-07-12 09:46:13 +0530

    Moving the competitive programming to a different directory.

--- Competitive Programming/Atcoder/counting.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	int a,b;cin>>a>>b;
-	(b>=a)?cout<<b-a+1<<endl:cout<<0<<endl;
-	return 0;
-}
--- DataStructures/Stack-Queue/slidingWidowsMaximum.cpp
+++ DataStructures/Stack-Queue/slidingWidowsMaximum.cpp
@@ -3,7 +3,21 @@
 #include<vector>
 using namespace std;
 
-int main(){
+void print(vetor<int>&arr) {
+	for(auto it:arr) cout<<it<<endl;
+	cout<<endl;
+}
+void slideIt(vector<int>&arr,int k){
 	
+}
+int main(){
+	int n;cin>>n;
+	vector<int>arr(n);
+	for(auto &it:arr) cin>>it;
+	cin>>n;
+	slideIt(arr,n);
+	print(arr);
+
+
 	return 0;
 }
