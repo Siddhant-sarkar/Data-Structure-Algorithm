4a816f66c9741f0008f7e5998e29ae9b15aa8d98
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-07-23 11:53:11 +0530

    iterative pre order post order traversal

--- DataStructures/GenericTree/prepostIterative.java
+++ DataStructures/GenericTree/prepostIterative.java
@@ -43,9 +43,34 @@ public class prepostIterative {
 
     return root;
   }
-
+  static class Pair{
+    Node node;
+    int state;
+    Pair(Node node,int state){
+      this.state=state;
+      this.node=node;
+    }
+  }
   public static void IterativePreandPostOrder(Node node) {
-    // write your code here
+    String pre="",pos="";
+    Stack<Pair> st= new Stack<>();
+    st.push(new Pair(node,-1));
+    while(st.size()>0){
+      Pair t= st.peek();
+      if(t.state==-1){
+        pre+=t.node.data+" ";
+        t.state++;
+      }else if(t.state==t.node.children.size()){
+        pos+=t.node.data+" ";
+        st.pop();
+      }else{
+        Pair np=new Pair(t.node.children.get(t.state),-1);
+        st.push(np);
+        t.state++;
+      }
+    }
+    System.out.println(pre);
+    System.out.println(pos);
   }
 
   public static void main(String[] args) throws Exception {
