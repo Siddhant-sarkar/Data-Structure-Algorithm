13b9f4db40bc03cdf34cb040f6c7f2573ae08910
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-07-31 11:23:41 +0530

    ONe last file changed

---
+++ DataStructures/BinarySearchTree/constructor.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class constructor {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr,int lo ,int hi) {
+    if(lo>hi){
+      return null;
+    }
+    int mid=(lo+hi)/2;
+    Node l = construct(arr,lo,mid-1);
+    Node r=construct(arr,mid+1,hi);
+    Node f=new Node(arr[mid],l,r);
+    return f;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr,0,arr.length-1);
+    display(root);
+    
+  }
+
+}
