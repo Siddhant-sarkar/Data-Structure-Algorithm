8273b24415c0ddc229e3fcc9f8e0456ed8be117e
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-08-15 20:07:34 +0530

    Tug of war question done and it was kinda easy

--- Algorithms/Recursion/tugOfWar.java
+++ Algorithms/Recursion/tugOfWar.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class tugOfWar {
 	static int mindiff = Integer.MAX_VALUE;
 	static String ans="";
+	static ArrayList<ArrayList<Integer>> ls= new ArrayList<>();
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n=scn.nextInt();
@@ -16,27 +17,29 @@ public class tugOfWar {
 	}
 	public static void solve(int[] arr, int vidx, ArrayList<Integer> set1, ArrayList<Integer> set2, int soset1, int soset2) {
 		if(vidx==arr.length){
-			if(Math.abs(soset1-soset2)<mindiff){
+			if(Math.abs(soset1-soset2)<mindiff && (set1.size()==set2.size() || Math.abs(set1.size()-set2.size())==1)){
 				mindiff=Math.abs(soset1-soset2);
-				ans+="[";
-				for(int a : set1){
-					ans+=a+",";
-				}
-				ans+="]";
-				ans+="[";
-				for(int a : set2){
-					ans+=a+",";
-				}
-				ans+="]";
+				// System.out.println(mindiff);
+				ans="";
+				ans+=set1;
+				ans+=" ";
+				ans+=set2;
 			}
+			// System.out.println(set1 +"||"+set2);
 			return;
 		}
-		set1.add(arr[vidx]);
-		solve(arr,vidx+1,set1,set2,soset1+arr[vidx],soset2);
-		set1.remove(set1.size()-1);
-		set2.add(arr[vidx]);
-		solve(arr,vidx+1,set1,set2,soset1,soset2+arr[vidx]);
-		set2.add(set2.size()-1);
+		if(set1.size()<(arr.length+1)/2){
+			set1.add(arr[vidx]);
+			solve(arr,vidx+1,set1,set2,soset1+arr[vidx],soset2);
+			set1.remove(set1.size()-1);
+			// if(set1.size()==0) return;
+		}
+		if(set2.size()<(arr.length+1)/2){
+			set2.add(arr[vidx]);
+			solve(arr,vidx+1,set1,set2,soset1,soset2+arr[vidx]);
+			set2.remove(set2.size()-1);
+			// if(set2.size()==0) return;
+		}
 	}
 
 }
