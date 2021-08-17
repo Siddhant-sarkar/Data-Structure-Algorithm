cf717167528d18f30fe5d75bba3ab404cabde319
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-08-17 11:42:28 +0530

    THis was so good

--- Algorithms/Recursion/QueenqueenCombi.java
+++ Algorithms/Recursion/QueenqueenCombi.java
@@ -3,32 +3,37 @@ import java.util.*;
 
 public class QueenqueenCombi {
 
+    public static void print(boolean [][]chess){
+        for(int i=0;i<chess.length;i++){
+            for(int j=0;j<chess[0].length;j++){
+                if(chess[i][j]) System.out.print("q\t");
+                else System.out.print("-\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int i, int j){
         if(qpsf==tq){
-            System.out.println("******");
-            for(int row=0;row<chess.length;row++){
-                for(int col=0;col<chess[0].length;col++){
-                    if(chess[row][col])System.out.print("q\t");
-                    else System.out.print("-\t");
-                }
-                System.out.println();
-            }
-            System.out.println("******");
+            print(chess);
             return;
         }
-        for(int row=i;row<chess.length;row++){
-            for(int col=j+1;col<chess[0].length;col++){
-                if(chess[i+1][j]==false){
-                    chess[row][col]=true;
-                    queensCombinations(qpsf+1,tq,chess,row,col);
-                    chess[row][col]=false;
-                }
+        for(int col=j+1;col<tq;col++){
+            chess[i][col]=true;
+            queensCombinations(qpsf+1,tq,chess,i,col);
+            chess[i][col]=false;
+        }
+        for(int row=i+1;row<tq;row++){
+            for(int col=0;col<tq;col++){
+                chess[row][col]=true;
+                queensCombinations(qpsf+1,tq,chess,row,col) ;
+                chess[row][col]=false;
             }
         }
     }
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
         boolean[][] chess = new boolean[n][n];
         
         queensCombinations(0, n, chess, 0, -1);
