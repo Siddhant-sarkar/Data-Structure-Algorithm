78ee5fcd6d10994be43086b4335383583286299a
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-08-20 20:57:49 +0530

    This is the correct method

--- Algorithms/BitManupulaiton/rSetBitMask.java
+++ Algorithms/BitManupulaiton/rSetBitMask.java
@@ -6,14 +6,8 @@ public class rSetBitMask {
   public static void main(String[] args){
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    long i=1;
-    while(n>0){
-      if(n%2==1) break;
-      n/=2;
-      i*=10;
-    }
-    System.out.println(i);
-
+    n= n&-n;
+    System.out.println(Integer.toBinaryString(n));
   }
 
 }
