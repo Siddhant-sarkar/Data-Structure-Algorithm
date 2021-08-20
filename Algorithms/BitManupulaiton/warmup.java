05bfa35fe8a1221e8c4cd14da9fb40a1a3de9063
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-08-20 08:26:07 +0530

    bit manipulation started

---
+++ Algorithms/BitManupulaiton/warmup.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class warmup {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int i = scn.nextInt();
+    int j = scn.nextInt();
+    int k = scn.nextInt();
+    int m = scn.nextInt();
+    int mask=1<<m;
+
+    System.out.println(n | (1<<i));
+    System.out.println(n &(~(1<<j)));
+    System.out.println(n ^ 1<<k);
+    System.out.println(((n & mask)==0)?false:true);
+  }
+
+}
