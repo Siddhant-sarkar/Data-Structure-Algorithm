afde6d773dbd34c9ab56c05eb0a949f92871bc15
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-08-27 11:35:07 +0530

    even more concise way of doing it

--- Algorithms/BitManupulaiton/countSetBits.java
+++ Algorithms/BitManupulaiton/countSetBits.java
@@ -9,24 +9,13 @@ public class countSetBits{
     System.out.println(solution(n));
   }
 
-  // public static int solution(int n){
-  //   int pow=1;
-  //   n++;
-  //   while(pow<n) pow<<=1;
-  //   int sum=0;
-  //   for(int i=2;i<pow;i*=2){
-  //     sum+=(n/i)*(i/2);
-
-  //   }
-
-  //   return sum;
-  // }
   public static int solution(int n){
-    if(n==1){
+    if(n==1 ){
       return 1;
     }
     int x=0,pow=1;
     while(pow <= n) { x++; pow*=2;}
+    System.out.println(pow+" "+x);
     x--;pow/=2;
     return ((pow/2)*x)+(n-pow+1)+solution(n-pow);
   }
