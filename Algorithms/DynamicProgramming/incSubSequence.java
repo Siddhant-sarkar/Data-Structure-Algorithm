7d9d6eea862b55f446cfc0912e0ccbd40e2ed94f
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-01 00:00:31 +0530

    the fuck i still wonder why it didn't worked for the first time

--- Algorithms/DynamicProgramming/incSubSequence.java
+++ Algorithms/DynamicProgramming/incSubSequence.java
@@ -1,41 +1,31 @@
-import java.io.*;
 import java.util.*;
 
-public class incSubSequence{
-
-   public static void main(String[] args) throws Exception {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      int[] arr = new int[n];
-      for (int i = 0; i < arr.length; i++) {
-         arr[i] = Integer.parseInt(br.readLine());
-      }
-
-      int omax = 0;
-      int[] dp = new int[arr.length];
-      for(int i = 0; i < arr.length; i++){
-         Integer max = null;
-
-         for(int j = 0; j < i; j++){
-            if(arr[j] <= arr[i]){
-               if(max == null || dp[j] > max){
-                  max = dp[j];
-               }
-            }
-         }
-
-         if(max != null){
-            dp[i] = max + 1;
-         } else {
-            dp[i] = 1;
-         }
-         
-         if(dp[i] > omax){
-            omax = dp[i];
-         }
-      }
-
-      System.out.println(omax);
-   }
-
+public class incSubsequence{
+	public static void liss(int[] arr){
+		int omax=0;
+		int[] dp = new int[arr.length];
+		for(int i=0;i<arr.length;i++){
+			int max=0;
+			for(int j=0;j<i;j++){
+				if(arr[i]>arr[j]){
+					if(dp[j]>max){
+						max= dp[j];
+					}
+				}
+			}
+			dp[i]=max+1;
+			if(omax<dp[i]) omax=dp[i];
+		}
+		for(int val:dp)System.out.println(val);
+		System.out.println(omax);
+	}
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		liss(arr);
+	}
 }
