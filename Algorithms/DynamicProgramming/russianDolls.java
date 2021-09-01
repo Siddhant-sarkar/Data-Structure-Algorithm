782ebbd11f091f2b8b27df4c91beebe2dc57ce57
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-01 19:51:29 +0530

    this was fucking great

---
+++ Algorithms/DynamicProgramming/russianDolls.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class russianDolls{
+	public static class Dolls implements Comparable<Dolls>{
+		int l;
+		int b;
+
+		Dolls(int l,int b){
+			this.l=l;
+			this.b=b;
+		}
+		public int compareTo(Dolls o){
+			return this.l-o.l;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n= Integer.parseInt(sc.nextLine());
+		Dolls[] dl = new Dolls[n];
+
+		for(int i=0;i<n;i++){
+			String line= sc.nextLine();
+			String [] arr= line.split(" ");
+			dl[i]=new Dolls(Integer.parseInt(arr[0]),Integer.parseInt(arr[1]));
+		}
+		Arrays.sort(dl);
+		// for(int i=0;i<dl.length;i++){
+		// 	System.out.println(dl[i].l+"  "+dl[i].b);
+		// }
+		// here goes the longest increasing subsequence part
+		int [] dp= new int[n];
+		int omax=0;
+		for(int i=0;i<dl.length;i++){
+			Integer max=null;
+			for(int j=0;j<i;j++){
+				if(dl[i].b>dl[j].b){
+					if(max==null || dp[j]>max)	max=dp[j];
+				}
+			}
+			dp[i]=(max==null)?1:max+1;
+			omax=(dp[i]>omax)?dp[i]:omax;
+		}
+		System.out.println(omax);
+	}
+
+}
