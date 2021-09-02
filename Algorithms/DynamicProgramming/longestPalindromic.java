d77a3583cffcec7bfc1e155bba5065c09a2db236
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-03 00:31:45 +0530

    Iam asking again who tf makes these kinds of questions

---
+++ Algorithms/DynamicProgramming/longestPalindromic.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class longestPalindromic{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s= sc.nextLine();
+		int dp[][] = new int[s.length()][s.length()];
+
+		for(int g=0;g<s.length();g++){
+			for(int i=0,j=g;j<dp.length;i++,j++){
+				if(g==0) dp[i][j]=1;
+				else if(g==1) dp[i][j]=(s.charAt(i)==s.charAt(j))?2:1;
+				else {
+					if(s.charAt(i)==s.charAt(j)){
+						dp[i][j]=dp[i+1][j-1]+2;
+					}else{
+						dp[i][j]= Math.max(dp[i][j-1],dp[i+1][j]);
+					}
+				}
+
+			}
+
+		}
+		System.out.println(dp[0][dp.length-1]);
+
+	}
+
+}
