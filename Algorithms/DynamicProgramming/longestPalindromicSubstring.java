8dcece2fa7d13c1126eec3ab028ae5171b24ac5a
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-03 12:20:41 +0530

    Fuck man dp are only fun in the hub not in real life

--- Algorithms/DynamicProgramming/longestPalindromicSubstring.java
+++ Algorithms/DynamicProgramming/longestPalindromicSubstring.java
@@ -4,9 +4,26 @@ import java.io.*;
 public class longestPalindromicSubstring{
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-		String s= s.nextLine();
+		String s= sc.nextLine();
 
-		int dp[][] = new int[s.length()][s.length()];
-		
+		boolean dp[][] = new boolean[s.length()][s.length()];
+		int maxg=0;
+		for(int g=0;g<s.length();g++){
+			for(int i=0,j=g;j<s.length();j++,i++)	{
+				if(j>=i){
+					if(g==0) {dp[i][j]=true;maxg=g;}
+					else if(g==1) {dp[i][j]=(s.charAt(i)==s.charAt(j))?true:false;if(dp[i][j]) maxg=g;}
+					else {
+						if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1] ){
+							dp[i][j]=true;
+							maxg=g;
+						}else{
+							dp[i][j]=false;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(maxg+1);	
 	}
 }
