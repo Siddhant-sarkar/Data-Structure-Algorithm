aabea34dd571dec2ae11dc1b09d0eb6ce5505cf6
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-03 11:41:44 +0530

    i am starting to get the hang of it

---
+++ Algorithms/DynamicProgramming/longestPalindromicSubstring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class longestPalindromicSubstring{
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		String s= s.nextLine();
+
+		int dp[][] = new int[s.length()][s.length()];
+		
+	}
+}
