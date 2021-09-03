54ef6c4dd87ca6e520111b2760f7dc8f9cf1dbe5
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-03 09:19:57 +0530

    who tf makes these questions

--- Algorithms/DynamicProgramming/longestPalindromic.java
+++ Algorithms/DynamicProgramming/longestPalindromic.java
@@ -7,22 +7,22 @@ public class longestPalindromic{
 		String s= sc.nextLine();
 		int dp[][] = new int[s.length()][s.length()];
 
-		for(int g=0;g<s.length();g++){
-			for(int i=0,j=g;j<dp.length;i++,j++){
-				if(g==0) dp[i][j]=1;
-				else if(g==1) dp[i][j]=(s.charAt(i)==s.charAt(j))?2:1;
-				else {
+		for(int g =0;g<s.length();g++){
+			for(int i=0,j=g;j<s.length();i++,j++){
+				if(g==0){
+					dp[i][j]=1;
+				}else if(g==1){
+					dp[i][j]=(s.charAt(i)==s.charAt(j))? 3:2;
+				}else{
 					if(s.charAt(i)==s.charAt(j)){
-						dp[i][j]=dp[i+1][j-1]+2;
+						dp[i][j]=dp[i][j-1]+dp[i+1][j]+1;
 					}else{
-						dp[i][j]= Math.max(dp[i][j-1],dp[i+1][j]);
+						dp[i][j]=dp[i][j-1]+dp[i+1][j]-dp[i+1][j-1];
 					}
 				}
-
 			}
-
 		}
-		System.out.println(dp[0][dp.length-1]);
+		System.out.println(dp[0][dp[0].length-1]);
 
 	}
 
