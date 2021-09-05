4bd79c7f878e564ec6fcfd1d0dc5a94cb3299726
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-05 12:25:14 +0530

    This is stilll in constructions

---
+++ Algorithms/DynamicProgramming/printAllTragerSumSubset.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class printAllTragerSumSubset{
+
+    public static class Pair{
+        int i;
+        int j;
+        String psf;
+
+        public Pair(int i, int j, String psf){
+            this.i = i;
+            this.j = j;
+            this.psf = psf;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int tar = Integer.parseInt(br.readLine());
+
+        //write your code here
+
+    }
+}
+
+
+                        
+
+
+                        
