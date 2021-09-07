730c00c1a2d65279234c70b8658166eb296e6e5c
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-08 01:21:08 +0530

    bracketPairs was easy

---
+++ Algorithms/DynamicProgramming/bracketPairs.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bracketPairs{
+	public static int bracketPairs(int ans){
+		int dp[]	= new int[dp+1];
+		dp[0]=1;
+		dp[1]=1;
+		for(int i=2;i<dp.length;i++){
+			for(int j=0;j<i;j++){
+				dp[i]+=dp[j]*dp[i-1-j];
+			}
+		}
+		return dp[dp.length-1];
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(bracketPairs(sc.nextInt()));
+	}
+}
