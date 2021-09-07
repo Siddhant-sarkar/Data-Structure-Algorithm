70de2b78e11f69d9237d8479c0e30519301855ac
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-07 23:51:19 +0530

    This was quite fun

---
+++ Algorithms/DynamicProgramming/catalan.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class catalan{
+	public static int Catalan(int n){
+		int dp[]= new int[n+1];
+		dp[0]=1;dp[1]=1;
+		for(int i=2;i<dp.length;i++){
+			for(int j=0;j<i;j++){
+				dp[i]+=dp[j]*dp[i-1-j];
+			}
+		}
+		return dp[dp.length-1];
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(Catalan(n));
+	}
+}
