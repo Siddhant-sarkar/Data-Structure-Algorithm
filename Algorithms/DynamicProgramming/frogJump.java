934ce33607e404bec94241b07b3ddad5930a2d4e
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-29 20:19:59 +0530

    used hashmap instead of array

--- Algorithms/DynamicProgramming/frogJump.java
+++ Algorithms/DynamicProgramming/frogJump.java
@@ -3,40 +3,34 @@ import java.util.*;
 
 public class frogJump {
 
-  public static boolean solution(int[] stones) {
-    boolean dp[]= new boolean[stones[stones.length-1]+1];
-    boolean sl[]=new boolean[stones[stones.length-1]+1];
-
-    for(int i:stones) sl[i]=true;
-    dp[0]=true;
-    int ls=1;
-
-    for(int i=1;i<dp.length;i++){
-      if(sl[i]){
-        if(i-(ls+1)>=0 && dp[i-ls-1]){
-          dp[i]=true;
-          ls=ls+1;
+    public static boolean solution(int[] stones) {
+        HashMap<Integer,HashSet<Integer>> mp = new HashMap<>();
+        for(int i:stones) mp.put(i,new HashSet<>());
+        mp.get(stones[0]).add(1);
+        for(int i:stones){
+            HashSet<Integer> op= mp.get(i);
+            System.out.println(i+" "+op);
+            // if(op.size()==0) return false;
+            for(int o:op){
+                int ns=i+o;
+                if(mp.containsKey(ns)){
+                    if(o-1!=0) mp.get(ns).add(o-1);
+                    mp.get(ns).add(o);
+                    mp.get(ns).add(o+1);
+                }
+            }
         }
-        else if(i-ls>=0 && dp[i-ls]){
-          dp[i]=true;
-        }else if(i-(ls-1)>=0 && dp[i-ls+1]) {
-            dp[i]=true;
-            ls=ls-1;
-        }
-      }
-    }
-    return dp[dp.length-1];
-  }
-
-  public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int[] arr = new int[n];
-    for (int i = 0 ; i < n; i++) {
-      arr[i] = scn.nextInt();
+        return mp.get(stones[stones.length-1]).size()!=0;
     }
 
-    System.out.println(solution(arr));
-  }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0 ; i < n; i++) {
+          arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
 
 }
