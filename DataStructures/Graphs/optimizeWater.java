6b47c7481760a4328df8ebfa84a42a11de01d290
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-10-25 08:04:10 +0530

    used prims algorithm of minimum spanning tree

---
+++ DataStructures/Graphs/optimizeWater.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class optimizeWater {
+  public static class Pair implements Comparable<Pair>{
+    int vtx;
+    int wt;
+    Pair(int vtx,int wt){
+      this.vtx=vtx;
+      this.wt=wt;
+    }
+    @Override
+    public int compareTo(Pair p){
+      return this.wt-p.wt;
+    }
+
+  }
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] st = br.readLine().split(" ");
+    int v = Integer.parseInt(st[0]);
+    int e = Integer.parseInt(st[1]);
+
+    int[] wells = new int[v];
+    String[] words = br.readLine().split(" ");
+
+    for (int i = 0; i < wells.length; i++) {
+      wells[i] = Integer.parseInt(words[i]);
+    }
+
+    int[][] pipes = new int[e][3];
+    for (int i = 0; i < e; i++) {
+      String[] st1 = br.readLine().split(" ");
+      pipes[i][0] = Integer.parseInt(st1[0]);
+      pipes[i][1] = Integer.parseInt(st1[1]);
+      pipes[i][2] = Integer.parseInt(st1[2]);
+
+    }
+
+    System.out.println(minCostToSupplyWater(v, wells, pipes));
+
+  }
+
+  public static int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+    ArrayList<ArrayList<Pair>>  graph= new ArrayList<>();
+    for(int i=0;i<=n;i++) graph.add(new ArrayList<>());
+    for(int i=0;i<pipes.length;i++){ // we are adding the pipes
+      int u=pipes[i][0];
+      int v=pipes[i][1];
+      int wt= pipes[i][2];
+      graph.get(u).add(new Pair(v,wt));
+      graph.get(v).add(new Pair(u,wt));
+    }
+    for(int i=1;i<n;i++){ // adding the wells -lines xD
+      graph.get(0).add(new Pair(i,wells[i-1]));
+      graph.get(i).add(new Pair(0,wells[i-1]));
+    }
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+    pq.add(new Pair(0,0));
+    boolean vis [] = new boolean[n+1];
+    int ans=0;
+    while(pq.size()>0){
+      Pair rm = pq.remove();
+      if(vis[rm.vtx]) continue;
+      ans+=rm.wt;
+      vis[rm.vtx]=true;
+      ArrayList<Pair> nbrs = graph.get(rm.vtx);
+      for(Pair nbr:nbrs){
+        if(vis[nbr.vtx]==false){
+          pq.add(nbr);
+        }
+      }
+    }
+    return ans;
+  }
+
+}
